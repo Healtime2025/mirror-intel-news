7818395bfb294f2e21c99007f5ff554d09fc509d
Healtime2025 <smoriti@gmail.com>
2025-06-01 21:45:21 +0200

    Update fetch-news.js

--- api/fetch-news.js
+++ api/fetch-news.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const apiKey = "1f59afb2e91949b2b63dcbe7ce834151";
 
   try {
-    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=za&pageSize=5&apiKey=${apiKey}`);
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=${apiKey}`);
     const data = await response.json();
 
     console.log("🧠 Mirror Intel Raw News Response:", data);
